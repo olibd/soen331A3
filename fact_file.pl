dd30cfa51da342d7f69ad37b35766de317c7d4b5
Maxwell Mowbray <mmowbray@mentlegen.com>
2015-03-29 17:05:22 -0400

    Added prolog files

---
+++ fact_file.pl
@@ -0,0 +1,13 @@
+% Author: O. Brochu Dufour, M. Mowbray
+% Date: 29-03-2015
+
+%% Facts
+
+%% Top-level states
+state(dormant).
+state(init).
+state(idle).
+state(monitoring).
+%% States within 
+
+%% eof
